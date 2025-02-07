69aadf7569d96889a797e5c2b832b5a817568ddd
hp <hp@DESKTOP-MOMO2M8>
2025-02-08 02:06:19 +0530

    deleting the test package

--- GitProj01/src/main/java/com/nt/test/App.java
+++ GitProj01/src/main/java/com/nt/test/App.java
@@ -9,7 +9,7 @@ public class App {
     public static void main(String[] args) {
     	
     	Arithmetic ar = new Arithmetic();
-    	int result=ar.sum(10, 20);
+    	long result=ar.sum(10, 20);
         System.out.println(result);
     }
 }
